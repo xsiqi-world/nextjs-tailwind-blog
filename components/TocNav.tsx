da44ef2cd3800f2692a647f0d80e68f7d64bdb7f
xsq <xsq_world@163.com>
2024-02-27 12:05:04 +0800

    style

--- components/TocNav.tsx
+++ components/TocNav.tsx
@@ -3,136 +3,136 @@
 // ref: https://github.com/ekomenyong/kommy-mdx/blob/main/src/components/TOC.tsx
 
 // import clsx from 'clsx';
-import GithubSlugger from 'github-slugger';
-import { useEffect, useRef, useState } from 'react';
+// import GithubSlugger from 'github-slugger';
+import {useEffect, useRef, useState} from 'react';
 
 // eslint-disable-next-line no-unused-vars
 type UseIntersectionObserverType = (setActiveId: (id: string) => void) => void;
 
 const useIntersectionObserver: UseIntersectionObserverType = (setActiveId) => {
-    const headingElementsRef = useRef<{
-        [key: string]: IntersectionObserverEntry;
-    }>({});
+  const headingElementsRef = useRef<{
+    [key: string]: IntersectionObserverEntry;
+  }>({});
 
-    useEffect(() => {
-        const callback = (headings: IntersectionObserverEntry[]) => {
-            headingElementsRef.current = headings.reduce((map, headingElement) => {
-                map[headingElement.target.id] = headingElement;
+  useEffect(() => {
+    const callback = (headings: IntersectionObserverEntry[]) => {
+      headingElementsRef.current = headings.reduce((map, headingElement) => {
+        map[headingElement.target.id] = headingElement;
 
-                return map;
-            }, headingElementsRef.current);
+        return map;
+      }, headingElementsRef.current);
 
-            const visibleHeadings: IntersectionObserverEntry[] = [];
+      const visibleHeadings: IntersectionObserverEntry[] = [];
 
-            Object.keys(headingElementsRef.current).forEach((key) => {
-                const headingElement = headingElementsRef.current[key];
-                if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
-            });
+      Object.keys(headingElementsRef.current).forEach((key) => {
+        const headingElement = headingElementsRef.current[key];
+        if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
+      });
 
-            const getIndexFromId = (id: string) =>
-                headingElements.findIndex((heading) => heading.id === id);
+      const getIndexFromId = (id: string) =>
+        headingElements.findIndex((heading) => heading.id === id);
 
-            if (visibleHeadings.length === 1) {
-                setActiveId(visibleHeadings[0].target.id);
-            } else if (visibleHeadings.length > 1) {
-                const sortedVisibleHeadings = visibleHeadings.sort(
-                    (a, b) => getIndexFromId(b.target.id) - getIndexFromId(a.target.id)
-                );
-
-                setActiveId(sortedVisibleHeadings[0].target.id);
-            }
-        };
-
-        const observer = new IntersectionObserver(callback, {
-            rootMargin: '0px 0px -70% 0px',
-        });
-
-        const headingElements = Array.from(
-            document.querySelectorAll('article h2,h3')
+      if (visibleHeadings.length === 1) {
+        setActiveId(visibleHeadings[0].target.id);
+      } else if (visibleHeadings.length > 1) {
+        const sortedVisibleHeadings = visibleHeadings.sort(
+          (a, b) => getIndexFromId(b.target.id) - getIndexFromId(a.target.id)
         );
 
-        headingElements.forEach((element) => observer.observe(element));
+        setActiveId(sortedVisibleHeadings[0].target.id);
+      }
+    };
 
-        return () => observer.disconnect();
-    }, [setActiveId]);
+    const observer = new IntersectionObserver(callback, {
+      rootMargin: '0px 0px -70% 0px',
+    });
+
+    const headingElements = Array.from(
+      document.querySelectorAll('article h2,h3')
+    );
+
+    headingElements.forEach((element) => observer.observe(element));
+
+    return () => observer.disconnect();
+  }, [setActiveId]);
 };
 
 type Source = {
-    value: string,
-    url: string,
-    depth: string | number
+  value: string,
+  url: string,
+  depth: string | number
 }
 
 type Props = {
-    // source: string;
-    source: Source[]
+  // source: string;
+  source: Source[]
 };
 
-const FloatNav = ({ source }: Props) => {
-    // const { t } = useTranslation(['common']);
+const TocNav = ({source}: Props) => {
+  // const { t } = useTranslation(['common']);
 
-    // const headingLines = source
-    //     .split('\n')
-    //     .filter((line) => line.match(/^###?\s/));
-    //
-    // const headings = headingLines.map((raw) => {
-    //     const text = raw.replace(/^###*\s/, '');
-    //     const level = raw.slice(0, 3) === '###' ? 3 : 2;
-    //     const slugger = new GithubSlugger();
-    //
-    //     return {
-    //         text,
-    //         level,
-    //         id: slugger.slug(text),
-    //     };
-    // });
+  // const headingLines = source
+  //     .split('\n')
+  //     .filter((line) => line.match(/^###?\s/));
+  //
+  // const headings = headingLines.map((raw) => {
+  //     const text = raw.replace(/^###*\s/, '');
+  //     const level = raw.slice(0, 3) === '###' ? 3 : 2;
+  //     const slugger = new GithubSlugger();
+  //
+  //     return {
+  //         text,
+  //         level,
+  //         id: slugger.slug(text),
+  //     };
+  // });
 
-    const headings = source.map((raw) => {
-        return {
-            text: raw.value,
-            id: raw.url.replace(/#*/, ''),
-            level: raw.depth,
-        }
-    })
+  const headings = source.map((raw) => {
+    return {
+      text: raw.value,
+      id: raw.url.replace(/#*/, ''),
+      level: raw.depth,
+    }
+  })
 
-    const [activeId, setActiveId] = useState<string>();
+  const [activeId, setActiveId] = useState<string>();
 
-    useIntersectionObserver(setActiveId);
+  useIntersectionObserver(setActiveId);
 
-    return (
-        <div className="mt-10">
-            <p className="mb-5 text-lg font-semibold text-gray-900 transition-colors dark:text-gray-100">
-                TABLE OF CONTENTS
-            </p>
-            <div className="flex flex-col items-start justify-start">
-                {headings.map((heading, index) => {
-                    return (
-                        <button
-                            key={index}
-                            type="button"
-                            className={[
-                                heading.id === activeId
-                                    ? 'font-medium text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
-                                    : 'font-normal text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200',
-                                heading.level === 3 && 'pl-4',
-                                'mb-3 text-left text-sm transition-colors hover:underline'
-                            ].join(' ')}
-                            onClick={(e) => {
-                                e.preventDefault();
-                                document.querySelector(`#${heading.id}`)?.scrollIntoView({
-                                    behavior: 'smooth',
-                                    block: 'start',
-                                    inline: 'nearest',
-                                });
-                            }}
-                        >
-                            {heading.text}
-                        </button>
-                    );
-                })}
-            </div>
-        </div>
-    );
+  return (
+    <div className="mt-10">
+      <p className="mb-5 text-lg font-semibold text-gray-900 transition-colors dark:text-gray-100">
+        TABLE OF CONTENTS
+      </p>
+      <div className="flex flex-col items-start justify-start">
+        {headings.map((heading, index) => {
+          return (
+            <button
+              key={index}
+              type="button"
+              className={[
+                heading.id === activeId
+                  ? 'font-medium text-primary-500 hover:text-primary-600 dark:hover:text-primary-400'
+                  : 'font-normal text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200',
+                heading.level === 3 && 'pl-4',
+                'mb-3 text-left text-sm transition-colors hover:underline'
+              ].join(' ')}
+              onClick={(e) => {
+                e.preventDefault();
+                document.querySelector(`#${heading.id}`)?.scrollIntoView({
+                  behavior: 'smooth',
+                  block: 'start',
+                  inline: 'nearest',
+                });
+              }}
+            >
+              {heading.text}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
-export default FloatNav;
+export default TocNav;
